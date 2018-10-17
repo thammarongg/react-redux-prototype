bba7cc3b46966ee58a87decef9dcf62fac0a5087
Thammarong Glomjai <thammrong@gmail.com>
2018-10-18 00:06:18 +0700

    create and update file

---
+++ src/components/NavAppBar/NavAppBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class NavAppBar extends Component {
+render() {
+  return (
+    <div className="NavAppBar">
+      Fucking Up...
+    </div>
+  )
+}
+}
+
+export default NavAppBar;
