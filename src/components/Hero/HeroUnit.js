45cf6f227f4acd149c857f1b03f7f0d884fba18b
Thammarong Glomjai <thammrong@gmail.com>
2018-11-03 21:42:06 +0700

    create redux

--- src/components/Hero/HeroUnit.js
+++ src/components/Hero/HeroUnit.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 
+import { connect } from 'react-redux'
+
+
+
 const styles = theme => ({
     heroUnit: {
         backgroundColor: theme.palette.background.paper,
@@ -16,23 +20,41 @@ const styles = theme => ({
         marginTop: theme.spacing.unit * 4,
     }
 })
-class HeroUnit extends Component {
-    render() {
-        const { classes } = this.props
-        return (
-            <div className={classes.heroUnit}>
+const { classes } = this.props
+const HeroUnit = ({message, counter}) => (
+    <div className={classes.heroUnit}>
                 <div className={classes.heroContent}>
                     <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
                         Album layout
                     </Typography>
                 </div>
             </div>
-        )
+    )
+
+// class HeroUnit extends Component {
+//     render() {
+//         const { classes } = this.props
+//         return (
+//             <div className={classes.heroUnit}>
+//                 <div className={classes.heroContent}>
+//                     <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+//                         Album layout
+//                     </Typography>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+const mapStateToProps = function(state) {
+    return {
+      message: 'This is message from mapStateToProps',
+      counter: state.counters || 0
     }
-}
+  }
 
 HeroUnit.PropTypes = {
     classes: PropTypes.object.isRequired,
 }
-
-export default withStyles(styles)(HeroUnit)
+const AppWithConnect = connect(mapStateToProps)(HeroUnit)
+export default withStyles(styles)(AppWithConnect)
